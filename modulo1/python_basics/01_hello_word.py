e8261a6fef326192621be6b30917385d9ec6ff3e
Karling23 <clalquinga@gmail.com>
2026-04-07 11:05:15 -0500

    clase 7/04/2026

---
+++ modulo1/python_basics/01_hello_word.py
@@ -0,0 +1,19 @@
+print("Hello, World!")
+
+edad = 25
+nombre = "Carlos"
+print(f"Mi nombre es {nombre} y tengo {edad} años.")
+
+print("Hola", "Pedro", "Perez")
+print("Nombre:", nombre, "Edad:", edad)
+
+telefono = 1234567890
+
+print(nombre, edad, telefono, sep = " , ")
+
+print( nombre, end = " | ")
+print(edad, end = " | ")
+print(telefono, end = " | ")
+
+print(f"{3.146616816:.2f}")
+print(f"{3146616816:,}")
---
+++ modulo1/python_basics/04_operadores.py
@@ -0,0 +1,26 @@
+#Operadores Matemáticos
+a = 15
+b = 20
+print("Suma")
+print(a,"+", b, "=", a + b)
+print("Resta")
+print(a,"-", b, "=", a - b)
+print("Multiplicación")
+print(a,"*", b, "=", a * b)
+print("División")
+print(a,"/", b, "=", a / b)
+print("Modulo")
+print(a,"%", b, "=", a % b)
+print("Operador de Asignación")
+print("======================")
+n= 10
+n += 5 
+print("n += 5:", n)
+n -= 3
+print("n -= 3:", n)
+n *= 2
+print("n *= 2:", n)
+n /= 2
+print("n /= 2:", n)
+n**= 2
+print("n **= 2:", n)
